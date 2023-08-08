f7821d86782058f002ae0b1bfc9fb611a9798e10
Marcus Holl <marcus.holl@sap.com>
2023-08-08 15:26:54 +0200

    troubleshoot writePipelineEnv

--- vars/writePipelineEnv.groovy
+++ vars/writePipelineEnv.groovy
@@ -12,10 +12,11 @@ void call(Map parameters = [:]) {
     }
 
     def jsonMap = groovy.json.JsonOutput.toJson(cpe)
+    script.echo("[MH] PIPER_pipelineEnv: ${jsonMap}")
     if (piperGoPath && jsonMap) {
         withEnv(["PIPER_pipelineEnv=${jsonMap}"]) {
             def output = script.sh(returnStdout: true, script: "${piperGoPath} writePipelineEnv")
-            if (parameters?.verbose) {
+            if (parameters?.verbose || true) {
                 script.echo("wrote commonPipelineEnvironment: ${output}")
             }
         }
